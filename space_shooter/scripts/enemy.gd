65f65ea18ed614f8cef35c0fde3c59cd51df9cb3
HA5UN0 <ha5un0xsama@gmail.com>
2025-05-05 01:30:35 -0400

    fixed bug in player script causing game to crash when player collided with enemy

--- space_shooter/scripts/enemy.gd
+++ space_shooter/scripts/enemy.gd
@@ -1,15 +1,15 @@
 extends Area2D
 
 @export var speed: float = 400
-@onready var mainScene = $'..'
+@onready var mainScene = $'../..'
 
 # movement
 func _process(delta):
 	position.y += speed * delta
 	
-	
-# enemy vs laser
 
+
+# enemy vs laser
 func _on_area_entered(area):
 	self.queue_free()
 	mainScene.score += 10
--- space_shooter/scripts/player.gd
+++ space_shooter/scripts/player.gd
@@ -17,4 +17,4 @@ func _process(_delta):
 func _on_area_entered(area):
 	if area.is_in_group("enemy"):
 		self.queue_free()
-		mainScene.is.game_over = true
+		mainScene.is_game_over = true
