111b09e1c7a98c291eb28a95f9647327499d96d0
HA5UN0 <ha5un0xsama@gmail.com>
2025-05-05 14:58:23 -0400

    created enemy sprite script that selects random enemy texture when spawned

--- space_shooter/scripts/enemy_spawner.gd
+++ space_shooter/scripts/enemy_spawner.gd
@@ -4,8 +4,14 @@ extends Node2D
 
 
 
+
 func _on_timer_timeout():
 	var new_enemy = enemy_tscn.instantiate()
+	
+	# var enemy_range = ["res://scenes/enemy.tscn", 2, 3, "Last"]
+	# var array = ["First", 2, 3, "Last"]
+
+	
 	self.add_child(new_enemy)
 	
 	var viewport_width = get_viewport_rect().size.x
